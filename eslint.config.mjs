781575e0718be7450f702a88944eeadd1a4b13b9
Andre <andresonda1995@gmail.com>
2026-06-29 10:53:01 -0600

    Install eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,21 @@
+// eslint.config.mjs
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import reactHooks from 'eslint-plugin-react-hooks';
+import prettier from 'eslint-config-prettier';
+
+export default tseslint.config(
+    js.configs.recommended,
+    ...tseslint.configs.recommended,
+    {
+        files: ['**/*.{js,jsx,ts,tsx}'],
+        plugins: {
+            'react-hooks': reactHooks,
+        },
+        rules: {
+            ...reactHooks.configs.recommended.rules,
+            '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+        },
+    },
+    prettier,
+);
